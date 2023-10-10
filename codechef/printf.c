0a932b566105f87ea0c662f951914a6f153e6e09
asutosh-mahapatra-123 <asutosh92005@gmail.com>
2023-10-11 00:25:28 +0530

    today i learnt about how to print a number

---
+++ codechef/printf.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+int main()
+{
+    // coment line
+    /* here in this program i learn about hoe to print values using printf
+    and various imp things about printf*/
+
+    printf("%d and %d", 3 + 4, 2 + 1);
+}
